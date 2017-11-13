36be9c6c1d050d8f80f3df9a9d3d3cdb3bcba199
jillzhouzh <32000082+jillzhouzh@users.noreply.github.com>
2017-11-12 19:25:13 -0600

    bank with capitalized choices

---
+++ transfromclass.py
@@ -0,0 +1,13 @@
+def main():
+	infile = open("question_bank.txt", "r")
+	outfile = open('transformedBank', 'w')
+	for line in infile:
+		line = eval(line)
+		for i in range(len(line)):
+			if i >= 2 and (type(line[i]) != type(True)):
+				line[i] = str(line[i]).upper()
+		outfile.write(str(line))
+		outfile.write("\n")
+	infile.close()
+	outfile.close()
+main()
